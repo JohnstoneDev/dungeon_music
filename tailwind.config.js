d4df2d12cc92ee8aad62f11c75f0b3822e37fbad
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-05 03:29:44 +0300

    style: Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
+    fontFamily : {
+      main : ['Work Sans', 'sans-serif'],
+      headings : ['Archivo', 'sans-serif'],
+      text : ['Ranade', 'sans-serif']
+
+    },
     extend: {},
   },
   plugins: [],
