e50b69f8e0e30448df1cdbadd094984c3d1efd2d
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-05 15:59:23 +0300

    feat: Dummy content

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+
+
+export const Home = () => {
+	return (
+		<div className="space-x-4">
+				<h1 className="text-8xl font-headings"> Dungeon Events </h1>
+				<p> Home Component </p>
+		</div>
+	)
+}
