2c4871fa0ec4a7d48babe61b11255c7d80182b2f
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-05 16:17:07 +0300

    feat: style App component, render Paths, create Application Context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { useEffect } from 'react';
-import './App.css';
+import {
+  useEffect,
+  // useState,
+  // useCallback,
+  // useContext,
+  createContext
+} from 'react';
+
+// Local Imports
+import { Paths } from './Components/Routes';
+
+const ApplicationContext = createContext();
 
 function App() {
   // const client_id = process.env.REACT_APP_SEAT_GEEK_CLIENT_ID;
   // const app_key = process.env.REACT_APP_SEAT_GEEK_KEY;
 
   useEffect(() => {
-    
+
   });
 
   return (
-    <div className="App text-2xl font-text">
-     <h1 className="font-headings text-6xl">
-        Hello There
-      </h1>
-      <p>
-        Slim Shady
-      </p>
+    <div
+      className
+      ="App text-2xl font-main text-gray-800 min-h-screen leading-loose bg-blend-darken
+      tracking-tighter bg-gradient-to-r from-cyan-100 via-violet-200 to-sky-200
+      absolute w-screen items-center flex flex-col space-y-4 gap-4 will-change-scroll">
+      <ApplicationContext.Provider>
+        <Paths />
+      </ApplicationContext.Provider>
     </div>
   );
 }
