e177ef8674b72817b27ddc9ddcdc2f04228f76a7
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-07 23:39:00 +0300

    feat: Render Performers & Venues Routes

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,17 +1,19 @@
 import {
 	Routes,
-	Route
-} from "react-router-dom"; 
-
-
+	Route,
+} from "react-router-dom";
 
 import { Home } from "./Home"
+import { Performers } from './Performers'
+import { Venues } from './Venues'
 
 export const Paths = () => {
 	return (
 		<div className="p-4 m-4 relative">
 			<Routes className>
-				<Route exact path="/" element={<Home />} />
+				<Route path="/" element={<Home />}/>
+					<Route path="venues" element={<Venues />} />
+					<Route path="performers" element={<Performers />} />
 			</Routes>
 		</div>
 	)
