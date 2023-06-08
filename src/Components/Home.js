df9ab744385302a56054917a63591124efc54a05
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-08 23:18:33 +0300

    feat: animate home component using framer motion

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,16 @@
+import { motion } from 'framer-motion'
+
 import { Events } from './Events'; 
 
+
 export const Home = () => {
+	
 	return (
 		<div className="flex flex-col gap-4 m-4 p-4">
 			<h1 className='font-headings'> Happening Soon :</h1>
-
-			<div>
-				<Events />
-			</div>
+			<motion.div  animate={{ opacity: 1 }}>
+					<Events />
+			</motion.div>
 		</div>
 	)
 }
