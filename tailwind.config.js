a3352e9ac7dfd2444475770e984d98d708ae4b2a
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-08 23:13:53 +0300

    feat: Screen sizes for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
       main : ['Work Sans', 'sans-serif'],
       headings : ['Archivo', 'sans-serif'],
       text : ['Ranade', 'sans-serif']
-
+    },
+    screens: {
+      'mobile' : '375px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
     },
     extend: {},
   },
